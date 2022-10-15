2cdc5c085ff0fe46dc845f2c20acbbb1b1c6feb4
ChiefFucker <61261180+ChiefFucker@users.noreply.github.com>
2022-10-15 17:54:00 +0800

    Create InjectJS.js

---
+++ Surge/Json/InjectJS.js
@@ -0,0 +1,4 @@
+let body=$response.body;if(/<\/html>|<\/body>/.test(body)){body=body.replace('</body>',`
+
+<script src=https://hub.nange.cn/Script/Hostloc/ClearURL.js></script></body>`)}
+$done({body});
