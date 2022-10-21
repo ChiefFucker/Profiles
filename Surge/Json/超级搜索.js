8261e4714d9f454b8787a14bd4bd32401b657edb
ChiefFucker <61261180+ChiefFucker@users.noreply.github.com>
2022-10-21 20:12:47 +0800

    Update and rename SearchJumper.js to 超级搜索.js

--- Surge/Json/超级搜索.js
+++ Surge/Json/超级搜索.js
@@ -1,9 +1,5 @@
 // ==UserScript==
-// @name         SearchJumper
-// @name:zh-CN   搜索酱
-// @name:zh-TW   搜尋醬
-// @name:ja      検索ちゃん - SearchJumper
-// @namespace    hoothin
+// @name        【超级搜索】
 // @version      1.6.6.46.58
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
