f1d25bf472c5ae4d349205a20f38f59bc93a16e0
ChiefFucker <61261180+ChiefFucker@users.noreply.github.com>
2023-11-21 18:14:16 +0800

    Update Adsense.js

--- Surge/Ruleset/Adsense.js
+++ Surge/Ruleset/Adsense.js
@@ -13,7 +13,7 @@ const noticeTitle = "广告联盟";
 
 if ((url.includes("api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk")
         || url.includes("api-access.pangolin-sdk-toutiao1.com/api/ad/union/sdk")
-        || url.includes("is.snssdk.com/api/ad/union/sdk") || url.includes("https://push.m.youku.com/collect-api/v1/guid_devices") || url.includes("sf3-fe-tos.pglstatp-toutiao.com/obj/ad-pattern/renderer/package.js"))
+        || url.includes("is.snssdk.com/api/ad/union/sdk") || url.includes("push.m.youku.com/collect-api/v1/guid_devices") || url.includes("sf3-fe-tos.pglstatp-toutiao.com/obj/ad-pattern/renderer/package.js"))
     && method === postMethod) {
     console.log('穿山甲-get_ads');
     if (!body.message) {
