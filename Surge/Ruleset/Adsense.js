8047b4c548bb3978e5a308d4db33e85ce069acc7
ChiefFucker <61261180+ChiefFucker@users.noreply.github.com>
2023-11-24 12:26:05 +0800

    Update Adsense.js

--- Surge/Ruleset/Adsense.js
+++ Surge/Ruleset/Adsense.js
@@ -34,7 +34,7 @@ if ((url.includes("api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk")
         };
         console.log('成功');
     }
-} else if (url.includes('mi.gdt.qq.com') || url.includes('yk-ssp.ad.youku.com') || url.includes('huichuan.sm.cn') || url.includes('(adim || lp).pinduoduo.com'))) && method === getMethod) {
+} else if (url.includes('mi.gdt.qq.com') || url.includes('yk-ssp.ad.youku.com') || url.includes('huichuan.sm.cn') || url.includes('(adim || lp).pinduoduo.com') && method === getMethod) {
     console.log('优量汇');
     if ('ret' in body) {
         if (body.ret === 0) {
