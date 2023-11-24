1a5b61dbd77f142d07c9ef332be21030831caf27
ChiefFucker <61261180+ChiefFucker@users.noreply.github.com>
2023-11-24 20:46:04 +0800

    优化

--- Surge/Json/InjectJS.js
+++
@@ -1,4 +0,0 @@
-let body=$response.body;if(/<\/html>|<\/body>/.test(body)){body=body.replace('</body>',`
-
-<script src=https://hub.nange.cn/Script/Hostloc/ClearURL.js></script></body>`)}
-$done({body});
--- Surge/Json/nPlayer.js
+++
@@ -1,3 +0,0 @@
-var url = $request.url.replace(/language=[A-Za-z0-9_-]+/ig, 'language=zh');
-
-$done({url: url, headers: $request.headers });
