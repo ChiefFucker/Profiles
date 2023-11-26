c0f2daa0568cc9153e0001c2f523df86f015efd6
ChiefFucker <61261180+ChiefFucker@users.noreply.github.com>
2023-11-26 15:09:16 +0800

    Update Adsense.js

--- Surge/Ruleset/Adsense.js
+++ Surge/Ruleset/Adsense.js
@@ -13,7 +13,7 @@ const noticeTitle = "广告联盟";
 
 if ((url.includes("api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk")
         || url.includes("api-access.pangolin-sdk-toutiao1.com/api/ad/union/sdk")
-        || url.includes("is.snssdk.com/api/ad/union/sdk") || url.includes("log-api.pangolin-sdk-toutiao-b.com/service/2/app_log") || url.includes("api-access.pangolin-sdk-toutiao-b.com/api/ad/union/sdk") || url.includes("push.m.youku.com/collect-api/v1/guid_devices"))
+        || url.includes("is.snssdk.com/api/ad/union/sdk") || url.includes("api-access.pangolin-sdk-toutiao-b.com/api/ad/union/sdk"))
     && method === postMethod) {
     console.log('穿山甲-get_ads');
     if (!body.message) {
@@ -34,7 +34,7 @@ if ((url.includes("api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk")
         };
         console.log('成功');
     }
-} else if (url.includes('mi.gdt.qq.com') || url.includes('mc.atm.youku.com') || url.includes('yk-ssp.ad.youku.com') || url.includes('huichuan.sm.cn') || url.includes('adim.pinduoduo.com') || url.includes('cad.youku.com') && method === getMethod) {
+} else if (url.includes('mi.gdt.qq.com') || url.includes('yk-ssp.ad.youku.com') || url.includes('huichuan.sm.cn') || url.includes('cad.youku.com') && method === getMethod) {
     console.log('优量汇');
     if ('ret' in body) {
         if (body.ret === 0) {
